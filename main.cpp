9e206cdb5b38be1eb85b9c80ba72b0c9866ec9b9
AndikaHartanta <andikahartanta1019@gmail.com>
2023-10-15 16:35:37 +0700

    menambahkan file main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream> // input output stream
+
+using namespace std;
+
+int main() //main function
+{
+    cout << "Nama : Andika Hartanta" << endl << endl;
+    cout << "NPM : 2310631170067" << endl << endl;
+    cout << "Kelas : 1A - Informatika" << endl << endl;
+    cout << "Cita-cita : Programmer" << endl << endl;
+    cout << "Motivasi Masuk Informatika : tertarik dan minat pemrograman" << endl;
+    return 0;
+}
